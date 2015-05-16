Correct average program

fmmlib2d_omp_FJ/test/search_time_aver.py
@@ -1,33 +1,33 @@
 import glob
-import numpy as np
-fw=open("result.txt",'a')
+#import numpy as np
+fw=open("result.txt",'w')
 
 size=[]
 time=[]
+num=[]
 
 for name in glob.glob('./output/output*'):
     pre='./output/output_'
     str=name[len(pre):len(name)]
     list=str.split('_')
-    size.append(list[0])
-    #fw.write(list[0]+' '+list[1]+' ')
     fr=open(name,'r')
     for line in fr:
         if 'time in fmm main' in line:
-            #fw.write(next(fr))
-            time.append(float(next(fr)))
-fr.close()
+            t=float(next(fr))
+    fr.close()
+    j=0
+    while j<len(size):
+        if list[0]==size[j]:
+            num[j]+=1
+            time[j]+=t
+            break
+        j+=1
+    if j==len(size):
+        size.append(list[0])
+        time.append(t)
+        num.append(1)
 
-while len(size) > 0:
-    index=[0]
-    sum=time[0]
-    for i in range(1,len(size)):
-        if size[i]==size[0]:
-           index.append(i)
-           sum+=time[i]
-    t=sum/len(index)
-    print('%s\t%g\n'%(size[0],sum/len(index)),file=fw)
-    size=np.delete(size,index,0)
-    time=np.delete(time,index,0)
+for i in range(len(size)):
+    print('%s\t%f\n'%(size[i],time[i]/num[i]),file=fw)
 
 fw.close()